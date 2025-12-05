0afc96d524df2471578b86d79a795d50f9ad52a1
Joao <48494671+JoaoPedro92@users.noreply.github.com>
2025-12-05 13:33:31 +0000

    Ajustes ao commit anterior

--- GameStore/app/src/main/java/pt/iade/ei/gamestore/ui/controllers/DLCcontroller.kt
+++ GameStore/app/src/main/java/pt/iade/ei/gamestore/ui/controllers/DLCcontroller.kt
@@ -12,7 +12,7 @@ val shopGamesDLCs = listOf(
     DLCData(5, 2, "FIFA Coins 1000", "EA FC 26 Coins are the main currency in EA Sports FIFA 26. This pack includes: 1000 coins.", 9.99, R.drawable.fifacoins),
     DLCData(6, 2, "Stadium Pack", "Unlock banners, fireworks, custom chants, and dynamic lighting for your stadium.", 14.99, R.drawable.fifa_stadium_dlc),
 
-    DLCData(7, 3, "Rise of Horror", "Rise of Horror plunges you into a terrifying world where survival depends on your strength and strategy.", 4.99, R.drawable.fishing_minecraft),
+    DLCData(7, 3, "Rise of Horror", "Rise of Horror plunges you into a terrifying world where survival depends on your strength and strategy.", 4.99, R.drawable.riseofhorror_minecraft),
     DLCData(8, 3, "Mine Mail", "Send items across the world like never before! This utility addon introduces a mailbox system that lets players deliver items to each other—perfect for multiplayer and Realms. Includes up to 612 unique mailbox designs with 36 variants and full color customization!", 9.99, R.drawable.minemail_minecraft),
     DLCData(9, 3, "Fishing++", "New fishing mechanics, advanced fish AI behaviors, complex aquarium systems, and many more inside this FISHING Add-On!", 6.99, R.drawable.fishing_minecraft)
 )
